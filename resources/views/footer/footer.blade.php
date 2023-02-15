6c7fa8d11635bd008a928ecd54036dd8353474fc
MChassanNajib <chassannajib@gmail.com>
2023-02-15 14:02:45 +0700

    feat [CN] : Add Home Page

---
+++ resources/views/footer/footer.blade.php
@@ -0,0 +1,66 @@
+<div class="footer-section container-fluid">
+    <div class="footer-container container">
+    <div class="row">
+      <div class="footer-left col-lg-6">
+          <ul>
+            <h2> <b>G</b>-Learning&nbsp;&nbsp;</b></h2>
+          <li>
+              <img src="images/location-icon.svg" alt="">
+              <span>Jalan Sukun Raya No. 9, Besito, Gebok, Besito, Gebog, Kabupaten Kudus, Jawa Tengah 59333, Indonesia.</span>
+          </li>
+          <li ><a href="https://r.search.yahoo.com/_ylt=AwrOsDzEQ.Njk84HrA9XNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1675867204/RO=10/RU=https%3a%2f%2fsmkrus.sch.id%2f/RK=2/RS=OliDp5hualQhgVe6tc_q0f0Ly24-">
+              <img src="images/email-icon-light.svg" alt="">
+              <span style="text-decoration: none;">smkrus.sch.id</span>
+            </a>
+          </li>
+          <li>
+              <img src="images/phone-icon.svg" alt="">
+              <span >62+ 812810208291</span>
+          </li>
+      </ul>
+      </div>
+      <div class="footer-right col-lg-2">
+           <ul>
+              <li class="list-title">Community</li>
+              <li><a href="#">Learners</a></li>
+              <li><a href="#">Partners</a></li>
+              <li><a href="#">Developers</a></li>
+              <li><a href="#">Beta Testers</a></li>
+              <li><a href="#">Translators</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Tech Blog</a></li>
+              <li><a href="#">Teaching Center</a></li>
+          </ul>
+      </div>
+      <div class="footer-right col-lg-2">
+          <ul>
+              <li class="list-title">Informations</li>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Kursus</a></li>
+              <li><a href="#">Promo</a></li>
+              <li><a href="prices.html">Paket Belajar</a></li>
+              
+          </ul>
+      </div>
+      <div class="footer-right col-lg-2">
+          <ul>
+              <li class="list-title">More</li>
+              <li><a href="#">Masuk</a></li>
+              <li><a href="#">Daftar</a></li>
+          </ul>
+      </div>
+    </div>
+    </div>
+    <div class="footer-of-footer container">
+        <ul>
+            <li class="copyrights">Copyright © 2022 for education</li>
+            <li>
+                <ul>
+                    <li><a href="#"><img src="images/facebook.png" alt="facebook"></a></li>
+                    <li><a href="#"><img src="images/instagram.png" alt="instagram"></a></li>
+                    <li><a href="#"><img src="images/linkedin.png" alt="linkedin"></a></li>
+                    <li><a href="#"><img src="images/twitter.png" alt="twitter"></a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
