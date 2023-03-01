fddc577e94f8518e4c728d3417b94ca523f1b44b
GanErnesta <karsakudus@gmail.com>
2023-03-01 12:51:25 +0700

    feat [SG] : Add Promo

---
+++ resources/views/promo.blade.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="promo.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script src="https://kit.fontawesome.com/cfbdd6673d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" 
+    integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Document</title>
+</head>
+<body>
+    <section id="course" class="sec-1">
+        <img src="assets/img/cntry.png" alt="">
+        <h1>Popular Category</h1>
+    </section>
+    <section id="course" class="sec-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                    <div class="card">
+                        <img src="assets/img/eng.png" alt="">
+                    </div>
+                </div>
+                <div class="col-1">
+                    <div class="card">
+                        <img src="assets/img/chi.png" alt="">
+                    </div>
+                </div>
+                <div class="col-1">
+                    <div class="card">
+                        <img src="assets/img/grm.png" alt="">
+                    </div>
+                </div>
+                <div class="col-1">
+                    <div class="card">
+                        <img src="assets/img/ind.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="course" class="sec-3">
+        <div class="wrapper">
+            <div class="search_box">
+                <div class="search_field">
+                  <input type="text" class="input" placeholder="Search course">
+              </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <br>
+
+    <section id="course" class="sec-4">
+        <div class="container">
+            <h2 class="jdl2">Course in Here</h2>
+            <div class="row">
+                <div class="col-2">
+                    <div class="card">
+                        <img src="assets/img/gb1.png" alt="">
+                        <div class="rating">
+                            <input type="radio" name="eng">
+                            <input type="radio" name="eng">
+                            <input type="radio" name="eng">
+                            <input type="radio" name="eng">
+                            <input type="radio" name="eng">
+                    </div>
+                    </div>
+                    
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb2.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="writ">
+                                <input type="radio" name="writ">
+                                <input type="radio" name="writ">
+                                <input type="radio" name="writ">
+                                <input type="radio" name="writ">
+                        </div>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb3.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="build">
+                                <input type="radio" name="build">
+                                <input type="radio" name="build">
+                                <input type="radio" name="build">
+                                <input type="radio" name="build">
+                        </div>
+                        </div>
+                    </div>
+    </section>
+    <section id="course" class="sec-4">
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb4.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="speak">
+                                <input type="radio" name="speak">
+                                <input type="radio" name="speak">
+                                <input type="radio" name="speak">
+                                <input type="radio" name="speak">
+                        </div>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb5.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="chin">
+                                <input type="radio" name="chin">
+                                <input type="radio" name="chin">
+                                <input type="radio" name="chin">
+                                <input type="radio" name="chin">
+                        </div>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb6.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="chi">
+                                <input type="radio" name="chi">
+                                <input type="radio" name="chi">
+                                <input type="radio" name="chi">
+                                <input type="radio" name="chi">
+                        </div>
+                        </div>
+                    </div>
+    </section>
+    <section id="course" class="sec-4">
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb7.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="jap">
+                                <input type="radio" name="jap">
+                                <input type="radio" name="jap">
+                                <input type="radio" name="jap">
+                                <input type="radio" name="jap">
+                        </div>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb8.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="french">
+                                <input type="radio" name="french">
+                                <input type="radio" name="french">
+                                <input type="radio" name="french">
+                                <input type="radio" name="french">
+                        </div>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb9.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="rus">
+                                <input type="radio" name="rus">
+                                <input type="radio" name="rus">
+                                <input type="radio" name="rus">
+                                <input type="radio" name="rus">
+                        </div>
+                        </div>
+                    </div>
+    </section>
+    <section id="course" class="sec-4">
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb10.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="can">
+                                <input type="radio" name="can">
+                                <input type="radio" name="can">
+                                <input type="radio" name="can">
+                                <input type="radio" name="can">
+                        </div>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb11.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="thai">
+                                <input type="radio" name="thai">
+                                <input type="radio" name="thai">
+                                <input type="radio" name="thai">
+                                <input type="radio" name="thai">
+                        </div>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="assets/img/gb12.png" alt="">
+                            <div class="rating">
+                                <input type="radio" name="kan">
+                                <input type="radio" name="kan">
+                                <input type="radio" name="kan">
+                                <input type="radio" name="kan">
+                                <input type="radio" name="kan">
+                        </div>
+                        </div>
+                    </div>
+    </section>
+    <div class="footer-section container-fluid">
+        <div class="footer-container container">
+        <div class="row">
+          <div class="footer-left col-lg-6">
+              <ul>
+                <h2> <b>G</b>-Learning&nbsp;&nbsp;</b></h2>
+              <li>
+                  <img src="images/location-icon.svg" alt="">
+                  <span>Jalan Sukun Raya No. 9, Besito, Gebok, Besito, Gebog, Kabupaten Kudus, Jawa Tengah 59333, Indonesia.</span>
+              </li>
+              <li ><a href="https://r.search.yahoo.com/_ylt=AwrOsDzEQ.Njk84HrA9XNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1675867204/RO=10/RU=https%3a%2f%2fsmkrus.sch.id%2f/RK=2/RS=OliDp5hualQhgVe6tc_q0f0Ly24-">
+                  <img src="images/email-icon-light.svg" alt="">
+                  <span style="text-decoration: none;">smkrus.sch.id</span>
+                </a>
+              </li>
+              <li>
+                  <img src="images/phone-icon.svg" alt="">
+                  <span >62+ 812810208291</span>
+              </li>
+          </ul>
+          </div>
+          <div class="footer-right col-lg-2">
+               <ul>
+                  <li class="list-title">Community</li>
+                  <li><a href="#">Learners</a></li>
+                  <li><a href="#">Partners</a></li>
+                  <li><a href="#">Developers</a></li>
+                  <li><a href="#">Beta Testers</a></li>
+                  <li><a href="#">Translators</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Tech Blog</a></li>
+                  <li><a href="#">Teaching Center</a></li>
+              </ul>
+          </div>
+          <div class="footer-right col-lg-2">
+              <ul>
+                  <li class="list-title">Informations</li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Kursus</a></li>
+                  <li><a href="#">Promo</a></li>
+                  <li><a href="prices.html">Paket Belajar</a></li>
+                  
+              </ul>
+          </div>
+          <div class="footer-right col-lg-2">
+              <ul>
+                  <li class="list-title">More</li>
+                  <li><a href="#">Masuk</a></li>
+                  <li><a href="#">Daftar</a></li>
+              </ul>
+          </div>
+        </div>
+        </div>
+        <div class="footer-of-footer container">
+            <ul>
+                <li class="copyrights">Copyright © 2022 for education</li>
+                <li>
+                    <ul>
+                        <li><a href="#"><img src="images/facebook.png" alt="facebook"></a></li>
+                        <li><a href="#"><img src="images/instagram.png" alt="instagram"></a></li>
+                        <li><a href="#"><img src="images/linkedin.png" alt="linkedin"></a></li>
+                        <li><a href="#"><img src="images/twitter.png" alt="twitter"></a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+    <script src="g.js"></script>
+
+</body>
+</html>
